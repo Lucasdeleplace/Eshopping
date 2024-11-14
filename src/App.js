96f5b898034678ed280c84d63d29cc34b7e08513
LucasDeleplace <Lucasdele59@gmail.com>
2024-11-14 15:50:01 +0100

    fix: bug infinite loading project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header.js"
+import "./App.css";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
     </div>
   );
 }
