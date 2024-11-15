5b6b4baf1507eab457babacec4205566de1fbfca
LucasDeleplace <Lucasdele59@gmail.com>
2024-11-15 11:03:47 +0100

    update: update pages with shopnow

--- src/pages/Acceuil.js
+++ src/pages/Acceuil.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Shopnow from "../components/Shopnow";
 
 function Acceuil() {
   return (
     <div className="acceuil">
       <Header />
+      <Shopnow />
     </div>
   );
 }
