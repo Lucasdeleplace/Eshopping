33d21eb8cfa36b3546a9a78a2983bd04f3a32fad
“Memphis-Blues” <memphisperrot@gmail.com>
2024-11-18 11:52:03 +0100

    màj du footer encore tout cassé =c

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,53 +3,53 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <footer class="footer">
-            <div class="footer-column fashion-column">
+        <footer className="footer">
+            <div className="footer-column_fashion-column">
                 <ul>
-                    <li><a href="fashion"><h3 id="fashiony">FASHION</h3></a></li>
-                    <li><a href="fashdet" id="fashdet" class="greya">Complete your style with awesome clothes from us.</a></li>
+                    <li><a href="link"><h3 id="fashiony" className="headera">FASHION</h3></a></li>
+                    <li><a href="link" id="fashdet" className="greya">Complete your style with awesome clothes from us.</a></li>
                 </ul>
-                <div class="social-icons">
-                    <a href="link">
+                <div className="social-icons">
+                    <a href="link" id="fb">
                         <img src="https://www.svgrepo.com/show/43168/facebook-logo.svg" alt="Facebook" width="50" height="50" />
                     </a>
-                    <a href="link">
+                    <a href="link" id="insta">
                         <img src="https://www.svgrepo.com/show/312385/instagram.svg" alt="Instagram" width="50" height="50" />
                     </a>
-                    <a href="link">
+                    <a href="link" id="twit">
                         <img src="https://www.svgrepo.com/show/327404/logo-twitter.svg" alt="Twitter" width="50" height="50" />
                     </a>
-                    <a href="link">
+                    <a href="link" id="linkedin">
                         <img src="https://www.svgrepo.com/show/341996/linkedin-in.svg" alt="LinkedIn" width="50" height="50" />
                     </a>
                 </div>
             </div>
             
-            <div class="footer-column_menu-column">
+            <div className="footer-column_menu-column">
                 <ul>
-                    <li><a href="compagny" id="compagny-link" class="headera">Company</a></li>
-                    <li><a href="about">About</a></li>
-                    <li><a href="contactus">Contact us</a></li>
-                    <li><a href="support">Support</a></li>
-                    <li><a href="careers">Careers</a></li>
+                    <li><a href="link" id="compagny-link" className="headera">Company</a></li>
+                    <li><a href="link" id="about" className="greya">About</a></li>
+                    <li><a href="link" id="contactus" className="greya">Contact us</a></li>
+                    <li><a href="link" id="support" className="greya">Support</a></li>
+                    <li><a href="link" id="careers" className="greya">Careers</a></li>
                 </ul>
             </div>
             
-            <div class="footer-column_quick-links-column">
+            <div className="footer-column_quick-links-column">
                 <ul>
-                    <li><a href="quicklink" class="headera">Quick Link</a></li>
-                    <li><a href="sharelock">Share Location</a></li>
-                    <li><a href="ordtrack">Orders Tracking</a></li>
-                    <li><a href="sizguide">Size Guide</a></li>
-                    <li><a href="faqs">FAQs</a></li>
+                    <li><a href="link" id="quicklink" class="headera">Quick Link</a></li>
+                    <li><a href="link" id="sharelock" className="greya">Share Location</a></li>
+                    <li><a href="link" id="ordtrack" className="greya">Orders Tracking</a></li>
+                    <li><a href="link" id="sizguide" className="greya">Size Guide</a></li>
+                    <li><a href="link" id="faqs" className="greya">FAQs</a></li>
                 </ul>
             </div>
             
-            <div class="footer-column_legal-column">
+            <div className="footer-column_legal-column">
                 <ul>
-                    <li><a href="legal" class="headera">Legal</a></li>
-                    <li><a href="Tandc">Terms & Conditions</a></li>
-                    <li><a href="privpol">Privacy Policy</a></li>
+                    <li><a href="link" id="legal" classname="headera">Legal</a></li>
+                    <li><a href="link" id="Tandc" className="greya">Terms & Conditions</a></li>
+                    <li><a href="link" id="privpol" className="greya">Privacy Policy</a></li>
                 </ul>
             </div>
         </footer>
